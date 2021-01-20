32276aabf4dbb780168720f07af63f2bd67f56c7
erikjulios <erikjulios96@gmail.com>
2021-01-20 14:38:33 +0700

    Fibonnaci number ready

--- fibbonaci.java
+++ fibbonaci.java
@@ -5,6 +5,9 @@ public static void main(String args[])
 Scanner jumlah = new Scanner(System.in);
 System.out.print("Masukan jumlah angka fibbonacci : ");
 int count = jumlah.nextInt();
+if (count == 0) {
+	count = 10;
+}
 
  int n1=0,n2=1,n3,i;    
  System.out.println("fibbonacci ke-1 = "+n1+"\nfibbonacci ke-2 = "+n2);   
