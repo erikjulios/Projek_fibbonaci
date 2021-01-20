d5c390d94c49a20e4959948e7f2ef60e0d23a1da
erikjulios <erikjulios96@gmail.com>
2021-01-20 15:09:22 +0700

    number ready

--- fibbonaci.java
+++ fibbonaci.java
@@ -2,14 +2,11 @@ import java.util.Scanner ;
 class fibbonaci{ 
 public static void main(String args[])  
 {    
-Scanner jumlah = new Scanner(System.in);
-System.out.print("Masukan jumlah angka fibbonacci : ");
-int count = jumlah.nextInt();
-if (count == 0) {
-	count = 10;
-}
+// Scanner jumlah = new Scanner(System.in);
+// System.out.print("Masukan jumlah angka fibbonacci : ");
+// int count = jumlah.nextInt();
 
- int n1=0,n2=1,n3,i;    
+ int n1=0,n2=1,n3,i,count = 20;    
  System.out.println("fibbonacci ke-1 = "+n1+"\nfibbonacci ke-2 = "+n2);   
     
  for(i=3;i<=count;++i)  
---
+++ prima.java
@@ -0,0 +1,21 @@
+public class prima { 
+     public static void main(String args[]) {       
+            for (int i=1; i <= 20; i++){
+            int bil=0;
+            for (int j=1; j<=i; j++){
+                if (i%j==0){
+                    bil=bil+1;
+                }
+            }
+            if (bil==2){
+                System.out.println(i+" = bilangan prima " );
+
+            }    
+            else{
+                System.out.println(i+" = bukan bilangan prima ");
+            }
+            }         
+         
+        } 
+    } 
+
